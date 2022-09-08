fc43b5892f66b7be2de9f445a3b539b1dab1126a
Evandro Machado <110360835+evandro-ribeiro@users.noreply.github.com>
2022-09-08 13:50:31 -0300

    Página de cadastro concluída

---
+++ cadastro-alurabooks/script.js
@@ -0,0 +1,29 @@
+async function buscaEndereco(cep) {
+    try {
+        var mensagemErro = document.getElementById('erro');
+
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCepConvertida = await consultaCep.json();
+        if (consultaCepConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
+
+        cidade.value = consultaCepConvertida.localidade;
+        logradouro.value = consultaCepConvertida.logradouro;
+        estado.value = consultaCepConvertida.uf;
+        bairro.value = consultaCepConvertida.bairro;
+
+        console.log(consultaCepConvertida);
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`;
+        console.log(erro);
+    }
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => buscaEndereco(cep.value));
